c147fc63d2af504be1ea1ae005fd3d043a266d24
JieChau Huang <jiechau@gmail.com>
2024-05-05 07:25:40 +0800

    update to go.sh 0,1,2,3

--- go.sh
+++ go.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # 
-# . ./go.sh
-# . ./go.sh 1 # write NAS
+# . ./go.sh 0 # sync Dropbox to local
+# . ./go.sh 1 # write NAS (need mac local mount: /Volumes/)
+# . ./go.sh 2 # write NAS, using intranet rsync 873
+# . ./go.sh 3 # write NAS, using internet, only ds918, no ds212
 #
 # 另外注意 UltraFit256/photo_,video_ 下面要有 it_exists.txt
 #
@@ -13,42 +15,49 @@ cd /Users/jiechau_huang/life_codes/exiftool_mac # friDay mac
 echo "$PWD"
 
 # ARG
-is_nas=3
-if [ "$#" -eq 1 ]; then
-  is_nas=$1 # it should be '1'
+is_nas=4
+if [ "$#" -ne 1 ]; then
+   echo "no arg!!!!!!!!!!!!!!!!!!!!!!!!"
+   return
+   #exit 0
+else
+   is_nas=$1 # it should be '1'
 fi
-echo "NAS=${is_nas}"
-#return
-#exit 0
+echo "args number = $#, OK"
+echo "is_nas=${is_nas} (should be 0,1,2,3)"
+echo ""
 
 
 # config/config_vars.txt
 . config/config_vars.txt
+# config/config_secrets.txt
+. config/config_secrets.txt
+
+echo "config/config_vars.txt"
 echo $program_dir_base
 echo $working_dir_base
 echo $moved_dir_base
 echo $problem_dir_base
-echo $dest_photo_dir_base
-echo $dest_video_dir_base
+echo $dest_video_dir_base # UltraFit256
+echo $dest_photo_dir_base # UltraFit256
 echo ""
 
+# 這些是 dropbox 的實際目錄
 # config/config_sourcedir.txt
 filename_sourcedir="config/config_sourcedir.txt"
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'sourcedir=($(<$filename_sourcedir))'
 
-#echo 'aaa'
-#echo $remote_918_photo_dir_base
-#echo 'aaa'
+echo $filename_sourcedir
+echo ${remote_918_video_dir_base}
+echo ${remote_918_photo_dir_base}
+echo ${remote_213_video_dir_base}
+echo ${remote_213_photo_dir_base}
+echo ""
 
-# . go 0 才會 # if sync to UltraFit256
+# . go.sh 0 
 #
-# if sync to UltraFit256
-if [ "$is_nas" -ne 0 ]; then
-   echo "no drobpx, -ne 0"
-   echo "args=${is_nas}"
-   echo ""
-
-else
+# sync from dropbox to UltraFit256
+if [ "$is_nas" -eq 0 ]; then
 
    tmp_idx=0
    for i in "${sourcedir[@]}"
@@ -121,45 +130,100 @@ else
       fi
 
    done
+   echo "is_nas=${is_nas} done"
 
 fi
 
 
-# . go 1 才會 # if sync to NAS
+# . go.sh 1 才會 # if sync to NAS
 #
 # if sync to NAS
-if [ "$is_nas" -ne 1 ]; then
-
-   echo "no sync, -ne 1"
-   echo "args=${is_nas}"
-   echo ""
-
-else
+if [ "$is_nas" -eq 1 ]; then
 
    # rsync UltraFit256 to 162
    echo ""
-   CHECKFILE="${remote_918_photo_dir_base}/it_exists.txt"
-   if [ -f "$CHECKFILE" ]; then
-      echo "918 photo exists: $CHECKFILE"
-      rsync -a --delete "${dest_photo_dir_base}/" "${remote_918_photo_dir_base}"
-   fi
-   CHECKFILE="${remote_213_photo_dir_base}/it_exists.txt"
-   if [ -f "$CHECKFILE" ]; then
-      echo "213 photo exists: $CHECKFILE"
-      rsync -a --delete "${dest_photo_dir_base}/" "${remote_213_photo_dir_base}"
-   fi
-   CHECKFILE="${remote_918_video_dir_base}/it_exists.txt"
+
+   CHECKFILE="/Volumes/${remote_918_video_dir_base}/it_exists.txt"
    if [ -f "$CHECKFILE" ]; then
       echo "918 video exists: ${CHECKFILE}"
-      rsync -a --delete "${dest_video_dir_base}/" "${remote_918_video_dir_base}"
-   fi
-   CHECKFILE="${remote_213_video_dir_base}/it_exists.txt"
-   if [ -f "$CHECKFILE" ]; then
-      echo "213 video exists: $CHECKFILE"
-      rsync -a --delete "${dest_video_dir_base}/" "${remote_213_video_dir_base}"
+      rsync -a --delete "${dest_video_dir_base}/" "/Volumes/${remote_918_video_dir_base}"
    fi
 
+   CHECKFILE="/Volumes/${remote_918_photo_dir_base}/it_exists.txt"
+   if [ -f "$CHECKFILE" ]; then
+      echo "918 photo exists: $CHECKFILE"
+      rsync -a --delete "${dest_photo_dir_base}/" "/Volumes/${remote_918_photo_dir_base}"
+   fi
+
+   CHECKFILE="/Volumes/${remote_213_video_dir_base}/it_exists.txt"
+   if [ -f "$CHECKFILE" ]; then
+      echo "213 video exists: $CHECKFILE"
+      rsync -a --delete "${dest_video_dir_base}/" "/Volumes/${remote_213_video_dir_base}"
+   fi
+
+   CHECKFILE="/Volumes/${remote_213_photo_dir_base}/it_exists.txt"
+   if [ -f "$CHECKFILE" ]; then
+      echo "213 photo exists: $CHECKFILE"
+      rsync -a --delete "${dest_photo_dir_base}/" "/Volumes/${remote_213_photo_dir_base}"
+   fi
+
+   echo "is_nas=${is_nas} done"
 fi
+
+
+# . go.sh 2 才會 # write NAS, using rsync 873
+#
+# if sync to NAS
+if [ "$is_nas" -eq 2 ]; then
+   # 918, strangly it's format is 192.168.123.163::video/video_latest
+   echo "--dry-run admin@192.168.123.163::${remote_918_video_dir_base}/it_exists.txt"
+   sshpass -p $pw rsync --port=873 -e "ssh -p 22" --dry-run --timeout=10 admin@192.168.123.163::${remote_918_video_dir_base}/it_exists.txt
+   if [ $? -eq 0 ]; then
+      sshpass -p $pw rsync --port=873 -e "ssh -p 22" -a --delete "${dest_video_dir_base}/" admin@192.168.123.163::${remote_918_video_dir_base}
+      echo "163::video" $?
+      sshpass -p $pw rsync --port=873 -e "ssh -p 22" -a --delete "${dest_photo_dir_base}/" admin@192.168.123.163::${remote_918_photo_dir_base} 
+      echo "163::photo" $?
+   else
+      echo "rsync 163 fail:" $?
+   fi
+   # 213, strangly it's format is 192.168.123.163:/volume1/video/video_latest
+   echo "--dry-run admin@192.168.123.162:/volume1/${remote_213_video_dir_base}/it_exists.txt"
+   sshpass -p $pw rsync --port=873 -e "ssh -p 22" --dry-run --timeout=10 admin@192.168.123.162:/volume1/${remote_213_video_dir_base}/it_exists.txt 
+   if [ $? -eq 0 ]; then
+      sshpass -p $pw rsync --port=873 -e "ssh -p 22" -a --delete "${dest_video_dir_base}/" admin@192.168.123.162:/volume1/${remote_213_video_dir_base} 
+      echo "162:/../video" $?
+      sshpass -p $pw rsync --port=873 -e "ssh -p 22" -a --delete "${dest_photo_dir_base}/" admin@192.168.123.162:/volume1/${remote_213_photo_dir_base}
+      echo "162:/../photo" $?
+   else
+      echo "rsync 162 fail:" $?   
+   fi
+   # test
+   #sshpass -p $pw rsync --port=873 -e "ssh -p 22" -a --delete /Users/jiechau_huang/tmp/DS918file/file_ttt/ admin@192.168.123.163::DS918file/file_ttt
+   echo
+   echo "is_nas=${is_nas} done"
+fi
+
+
+# . go.sh 3 # write NAS, using public, only ds918, no ds212
+#
+# if sync to NAS
+if [ "$is_nas" -eq 3 ]; then
+   # 918
+   # 918
+   echo "--dry-run admin@$pi::${remote_918_video_dir_base}/it_exists.txt"
+   sshpass -p $pw rsync --port=$pp -e "ssh -p $pp" --dry-run --timeout=10 admin@$pi::${remote_918_video_dir_base}/it_exists.txt
+   if [ $? -eq 0 ]; then
+      sshpass -p $pw rsync --port=$pp -e "ssh -p $pp" -a --delete "${dest_video_dir_base}/" admin@$pi::${remote_918_video_dir_base} 
+      echo "$pi::video" $?
+      sshpass -p $pw rsync --port=$pp -e "ssh -p $pp" -a --delete "${dest_photo_dir_base}/" admin@$pi::${remote_918_photo_dir_base} 
+      echo "$pi::photo" $?
+   else
+      echo "rsync $pi fail:" $?   
+   fi
+   echo
+   echo "is_nas=${is_nas} done"
+fi
+
 
 # end
 echo "ddone"
